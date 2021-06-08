8d343df70d7d5af31356a4869b780df97bfb0be3
Mephysics <ichigoez0@gmail.com>
2021-06-08 22:22:01 +0700

    adding labels

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,25 @@ import { Probot } from "probot";
 
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
-    const username = context.payload.sender.login
+    const username = context.payload.sender.login;
     const issueComment = context.issue({
-      body: `Halo @${username} terima kasih sudah mengirimkan issues`,
+      body: `Halo @${username} terima kasih sudah melaporkan, anda akan mendapatkan notifikasih setalah kami meninjau issues.`,
     });
-    console.log('comment created')
+    console.log('Issues created')
     await context.octokit.issues.createComment(issueComment);
+  });
+
+  app.on("issues.closed", async (context) => {
+    const username = context.payload.sender.login
+    const issueClosed = context.issue({
+      body: `Issues ditutup oleh @${username}.`
+    });
+    console.log('Issues closed')
+    await context.octokit.issues.createComment(issueClosed)
+    await context.octokit.issues.addLabels(
+      context.issue({
+        labels: ['closed']
+      })
+    )
   });
 };
