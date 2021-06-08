72bdc9cb61b2230d1a4809aa52d95b0a93ea8f82
Mephysics <ichigoez0@gmail.com>
2021-06-08 20:22:36 +0700

    client update

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { Probot } from "probot";
 
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
+    const username = context.payload.sender.login
     const issueComment = context.issue({
-      body: "Hello, World!",
+      body: `Halo @${username} terima kasih sudah mengirimkan issues`,
     });
+    console.log('comment created')
     await context.octokit.issues.createComment(issueComment);
   });
 };
