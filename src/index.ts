f3b9dc2e62e3f09c97df44e4a6f2b0781c8873b5
Ryu <ichigoez0@gmail.com>
2021-06-10 22:08:21 +0700

    client update

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,76 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
+
   app.on("issues.opened", async (context) => {
+    if (context.payload.sender.login === 'Mephysics') return
     const username = context.payload.sender.login;
     const issueComment = context.issue({
-      body: `Halo @${username} terima kasih sudah melaporkan, anda akan mendapatkan notifikasi setalah kami meninjau issues.`,
-    });
+      body: `Hello @${username} Thank you for submitting Issues, please wait for next notification after we review your Issues.`,
+    })
     console.log('Issues created')
-    await context.octokit.issues.createComment(issueComment);
     await context.octokit.issues.addLabels(
       context.issue({
         labels: ['pending']
       })
     )
+    await context.octokit.issues.createComment(issueComment)
   });
 
   app.on("issues.closed", async (context) => {
     const username = context.payload.sender.login
     const issueClosed = context.issue({
       body: `Issues ditutup oleh @${username}.`
-    });
+    })
     console.log('Issues closed')
+    await context.octokit.issues.addLabels(
+      context.issue({
+        labels: ['closed']
+      })
+    )
+    await context.octokit.issues.removeLabel(
+      context.issue({
+        name: 'pending'
+      })
+    )
+    if (context.payload.sender.login === 'Mephysics') return
     await context.octokit.issues.createComment(issueClosed)
+  });
+
+  app.on("pull_request.opened", async (context) => {
+    if (context.payload.sender.login === 'Mephysics') return
+    const username = context.payload.sender.login
+    const propened = context.issue({
+      body: `Hello @${username} Thank you for submitting Pull Request, please wait for next notification after we review your Pull Request`
+    })
+    console.log('Pull request opened')
+    await context.octokit.issues.createComment(propened)
+    await context.octokit.issues.addLabels(
+      context.issue({
+        labels: ['pending']
+      })
+    )
+    if (context.payload.pull_request.state === 'failure') { 
+      const Failedcomment = context.issue({
+        body: `@${username} CI Failed, Please fix code before merge.`
+      })
+      await context.octokit.issues.createComment(Failedcomment)
+      await context.octokit.issues.addLabels(
+        context.issue({
+          labels: ['CI Failed']
+        })
+      )
+    }
+  });
+
+  app.on("pull_request.closed", async (context) => {
+    if (context.payload.sender.login === 'Mephysics') return
+    const username = context.payload.sender.login
+    const prClosed = context.issue({
+      body: `Pull request ditutup oleh @${username}.`
+    })
+    console.log('Pull request closed')
+    await context.octokit.issues.createComment(prClosed)
     await context.octokit.issues.addLabels(
       context.issue({
         labels: ['closed']
@@ -33,4 +82,6 @@ export = (app: Probot) => {
       })
     )
   });
+
+
 };
