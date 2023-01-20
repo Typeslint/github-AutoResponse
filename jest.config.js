ee597cb958755aa9f1d9a21443124607295cbd94
Muunatic <ichigoez0@gmail.com>
2023-01-20 09:36:40 +0700

    chore: remove unnecessary extensions

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest"
   },
   testRegex: "(/__tests__/.*|\\.(test|spec))\\.[tj]sx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleFileExtensions: ["ts", "js", "json"],
   testEnvironment: "node"
 };
