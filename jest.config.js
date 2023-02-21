b393b943537610e7d384bbe531759311c47f2337
Muunatic <ichigoez0@gmail.com>
2023-02-21 22:24:48 +0700

    chore: remove unused test

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  roots: ["<rootDir>/src/", "<rootDir>/test/"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  testRegex: "(/__tests__/.*|\\.(test|spec))\\.[tj]sx?$",
-  moduleFileExtensions: ["ts", "js", "json"],
-  testEnvironment: "node"
-};
