3a586bf930eca6375f4803d997a2e03635e6ea4b
Muunatic <ichigoez0@gmail.com>
2023-11-13 12:23:22 +0700

    feat: create constant to export class

---
+++ src/structures/constant.ts
@@ -0,0 +1,21 @@
+import IssuesClose from "../class/issuesClose";
+import IssuesComment from "../class/issuesComment";
+import IssuesOpen from "../class/issuesOpen";
+import PullRequestOpen from "../class/prsOpen";
+import PullRequestReview from "../class/prsReview";
+import PullRequestSynchronize from "../class/prsSynchronize";
+import Push from "../class/push";
+import WorkflowCheck from "../class/workflowCheck";
+import PRsStale from "../class/prsStale";
+
+export {
+    IssuesClose,
+    IssuesComment,
+    IssuesOpen,
+    PullRequestOpen,
+    PullRequestReview,
+    PullRequestSynchronize,
+    Push,
+    WorkflowCheck,
+    PRsStale
+};
